c3885941a6525e686267fd82fbb522b00f4ab4d9
Pranav Dhangar <113121206+pranavv04@users.noreply.github.com>
2025-02-22 10:40:17 +0530

    Update Info.js

--- src/info/Info.js
+++ src/info/Info.js
@@ -4,7 +4,7 @@ import mock2 from "../img/mock2.png"
 import mock3 from "../img/mock3.png"
 import mock4 from "../img/mock4.png"
 import mock5 from "../img/mock5.png"
-
+import mock5 from "../img/mock44.png"
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
 
@@ -108,7 +108,7 @@ export const info = {
             title: "Socials",
             live: "https://socials-puce-six.vercel.app/login",
             source: "https://github.com/pranavv04/Socials",
-            image: mock5
+            image: mock44
         },
         {
             title: "NFT Marketplace",
